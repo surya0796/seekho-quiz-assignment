9d3cd3a2bde96e98d54dfe49ace2532244dad47a
Surya Saini <surya0796@gmail.com>
2023-03-06 03:15:10 +0530

    final score

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
   const [currQuestion, setCurrQuestion] = useState(0);
   const [result, setResult] = useState({});
   const [clickedList, setClickedList] = useState({});
+  const [showResult, setShowResult] = useState(null);
 
   const handleIncrement = (value) => {
     if (currQuestion < questions.length - 1) {
@@ -55,43 +56,73 @@ function App() {
     }
     setResult({ ...result });
   };
-
+  const handSubmit = () => {
+    let rightAnswers = 0;
+    Object.values(result).forEach((answer) => {
+      if (answer.isCorrect) {
+        rightAnswers += 1;
+      }
+    });
+    const finalScore = Math.round((rightAnswers / questions.length) * 100);
+    const wrongAnswers = questions.length - rightAnswers;
+    setShowResult({
+      correct: rightAnswers,
+      incorrect: wrongAnswers,
+      result: finalScore,
+    });
+  };
   return (
     <div className="quiz-app">
       <h1 className="quiz__app__heading">Let's Quizy</h1>
-      <div className="quiz__app__container">
-        <div className="quiz__layout quiz__app__result">
-          <RealTimeResult result={result} />
-        </div>
-        <div className="quiz__layout quiz__app__inner">
-          <div className="next__previous">
-            <button
-              className="nav__btn prev__btn"
-              onClick={() => handleDecrement(1)}
-            >
-              Previous
-            </button>
-            <button
-              className="nav__btn next__btn"
-              onClick={() => handleIncrement(1)}
-            >
-              Next
-            </button>
+      {!showResult ? (
+        <div className="quiz__app__container">
+          <div className="quiz__layout quiz__app__result">
+            <RealTimeResult result={result} />
           </div>
-          <Question
-            question={questions[currQuestion]}
+          <div className="quiz__layout quiz__app__inner">
+            <div className="next__previous">
+              {currQuestion > 0 && (
+                <button
+                  className="nav__btn prev__btn"
+                  onClick={() => handleDecrement(1)}
+                >
+                  Previous
+                </button>
+              )}
+              {currQuestion < questions.length - 1 && (
+                <button
+                  className="nav__btn next__btn"
+                  onClick={() => handleIncrement(1)}
+                >
+                  Next
+                </button>
+              )}
+            </div>
+            <Question
+              question={questions[currQuestion]}
+              currQuestion={currQuestion}
+              handleChange={handleChange}
+              result={result}
+            />
+            {Object.keys(clickedList).length === questions.length && <button className="nav__btn" onClick={handSubmit}>
+              Submit
+            </button>}
+          </div>
+          <QuestionNav
+            clickedList={clickedList}
             currQuestion={currQuestion}
-            handleChange={handleChange}
-            result={result}
+            handleQuestionClick={handleGoToQuestionByNumber}
+            questions={allQuestion}
           />
         </div>
-        <QuestionNav
-          clickedList={clickedList}
-          currQuestion={currQuestion}
-          handleQuestionClick={handleGoToQuestionByNumber}
-          questions={allQuestion}
-        />
-      </div>
+      ) : (
+        <div className="quiz__result quiz__layout">
+          <h2>Final Results Are :</h2>
+          <div>{showResult.correct}</div>
+          <div>{showResult.incorrect}</div>
+          <div>{showResult.result}%</div>
+        </div>
+      )}
     </div>
   );
 }
